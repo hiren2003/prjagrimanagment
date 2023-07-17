de691bfb4f06831afab28611ba6c0b377c7f935c
Kishan Bhuva <kishanbhuva102@gmail.com>
2023-07-17 21:38:23 +0530

    new commit

--- prjagrimanagment-master/AgricultureManagmentApp/app/src/main/java/com/project/agriculturemanagmentapp/SplashActivity.java
+++ prjagrimanagment-master/AgricultureManagmentApp/app/src/main/java/com/project/agriculturemanagmentapp/SplashActivity.java
@@ -2,7 +2,10 @@ package com.project.agriculturemanagmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Window window=this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.statusbarmain));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
     }
 }
